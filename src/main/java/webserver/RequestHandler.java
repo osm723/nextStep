e125d6bbdf9858ee649f7ea27066ce4e725907e0
oh <osm723@gmail.com>
2024-05-06 20:49:26 +0900

    수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,14 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -22,9 +27,7 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
 
-            InputStreamReader reader = new InputStreamReader(in);
-
-            BufferedReader br = new BufferedReader(reader);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 
             String line = br.readLine();
 
@@ -34,11 +37,62 @@ public class RequestHandler extends Thread {
 
             String[] token =  line.split("GET /");
 
+            while (!line.equals("")) {
+                line = br.readLine();
+                log.info("header======== {}", line);
+            }
+
             for (int i = 0; i < token.length; i++) {
                 String filePath = token[i].replace(" HTTP/1.1","");
                 if (!filePath.isEmpty()) {
                     log.info("filePath====== {}", filePath);
+
+                    if (filePath.contains("user/create")) {
+
+                        if (filePath.contains("POST")) {
+                            String val = IOUtils.readData(br, 1000);
+                            log.info("oh POST========== {}", val);
+                            Map<String, String> map = HttpRequestUtils.parseQueryString(val);
+
+                            log.info("oh userId========== {}", map.get("userId"));
+                            log.info("oh password========== {}", map.get("password"));
+                            log.info("oh name========== {}", map.get("name"));
+                            log.info("oh email========== {}", map.get("email"));
+
+                            User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+
+                            DataBase.addUser(user);
+
+                            filePath = "index.html";
+
+                            byte[] body = Files.readAllBytes(new File("./webapp/" + filePath).toPath());
+
+                            //
+                            DataOutputStream dos = new DataOutputStream(out);
+                            response302Header(dos, body.length);
+                            responseBody(dos, body);
+
+                            return;
+
+                        } else {
+                            int index = filePath.indexOf("?");
+                            String requestPath = filePath.substring(0,index);
+                            String parameter = filePath.substring(index+1);
+
+                            Map<String, String> map = HttpRequestUtils.parseQueryString(parameter);
+                            log.info("filePath2====== {}", map.toString());
+
+                            User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+
+                            DataBase.addUser(user);
+
+                            filePath = "index.html";
+                        }
+                    }
+
                     byte[] body = Files.readAllBytes(new File("./webapp/" + filePath).toPath());
+
+                    //
                     DataOutputStream dos = new DataOutputStream(out);
                     response200Header(dos, body.length);
                     responseBody(dos, body);
@@ -58,6 +112,17 @@ public class RequestHandler extends Thread {
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
